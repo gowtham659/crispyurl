1672cc78845333f8a5637d0ea1acf4695759c577
Gowtham Thota <gowthamthota121@gmail.com>
2025-08-03 11:45:59 +0530

    updated main

--- server/main.go
+++ server/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-	"urlshortnerreactgo/handlers"
+	handler "urlshortnerreactgo/handlers"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func main(){
+func main() {
 	app := fiber.New()
 	//hadling cors
 	//app.Use(cors.New())
 
 	app.Use(cors.New(cors.Config{
-    AllowOrigins: "http://localhost:3000/, https://react-go-oracle-app-1.onrender.com/",
-    AllowHeaders: "Origin, Content-Type, Accept",
-}))
-	app.Get("/",func (c *fiber.Ctx) error {
+		AllowOrigins: "http://localhost:3000/, https://react-go-oracle-app-1.onrender.com/",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("request success")
 	})
-	app.Post("/shorten",handler.ShortenUrl)
-	app.Use("/:shortcode",handler.RedirectURL)
+	app.Post("/shorten", handler.ShortenUrl)
+	app.Use("/:shortcode", handler.RedirectURL)
 	app.Listen(":8989")
 }
